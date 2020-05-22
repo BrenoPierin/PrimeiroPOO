80d3bc43d33563f781b3b2fa7d7587358fa9a3c8
unknown <brenomax02@gmail.com>
2020-05-22 10:51:32 -0300

    Projeto finalizado

---
+++ personagem.cs
@@ -0,0 +1,20 @@
+namespace Senai
+{
+    public class personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+
+        public int atacar ()
+        {
+           return  20 * 70;
+        }
+
+        public string defender()
+        {
+            return "O personagem defendeu!!";
+        }
+
+    }
+}
